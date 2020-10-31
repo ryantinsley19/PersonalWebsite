bc76472c3c9c6a0825c943d7009b5bef4f152e34
rdtp67 <rdtp67@gmail.com>
2020-10-31 15:45:56 -0500

    About Me - General Layout/Picture/Card

--- personwebsite_react/src/containers/about.js
+++ personwebsite_react/src/containers/about.js
@@ -1,9 +1,32 @@
 import React, {Component} from "react";
+import { Card, Container, Row, Image } from "react-bootstrap";
+import '../components/common/common.css'
+import SelfPic from "../assets/images/selfShotTemp.jpg";
 
 export default class About extends Component{
     render(){
         return(
-            <div>About</div>
+            <div className="about">
+                <div className="component-body">
+                    <Container>
+                        <Row className="justify-content-md-center">
+                            <Card style={{width: '30rem'}}>
+                                <div className="card-body">
+                                    <h3 className="card-title">Amount Me</h3>
+                                    <Card.Img className="card-image" variant="top"src={SelfPic}/>
+                                    <p>Mama, just killed a man
+                                        Put a gun against his head, pulled my trigger, now he's dead
+                                        Mama, life had just begun
+                                        But now I've gone and thrown it all away
+                                        Mama, ooh, didn't mean to make you cry
+                                        If I'm not back again this time tomorrow
+                                        Carry on, carry on as if nothing really matters</p>
+                                </div>
+                            </Card>
+                        </Row>
+                    </Container>
+                </div>
+            </div>
         );
     }
 }
