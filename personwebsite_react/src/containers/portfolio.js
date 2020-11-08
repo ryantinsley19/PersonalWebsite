f45a42e409f358d4495dc2e1861313b2a6a71005
rdtp67 <rdtp67@gmail.com>
2020-11-08 09:36:27 -0600

    Portfolio - General layout

--- personwebsite_react/src/containers/portfolio.js
+++ personwebsite_react/src/containers/portfolio.js
@@ -1,19 +1,59 @@
 import React, { Component } from "react";
+import { Card, Container, CardDeck } from "react-bootstrap";
 
 export default class Portfolio extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      item1: {
+        title: "title1",
+        body: "body 1 info blah blah blah",
+        footer: "footer1",
+      },
+      item2: {
+        title: "title2",
+        body: "body 2 info blah blah blah",
+        footer: "footer2",
+      },
+      item3: {
+        title: "title3",
+        body: "body 3 info blah blah blah",
+        footer: "footer3",
+      },
+    };
+  }
+
   render() {
+    const con = this.state;
     return (
-      <div>
-        <h1>Portfolio</h1>
-        <TestTitle title="Breakfast" />
-        <button onClick={testf}></button>
+      <div className="portfolio">
+        <div className="component-body">
+          <Container>
+            <CardDeck>
+              <CardBox content={con.item1} />
+              <CardBox content={con.item2} />
+              <CardBox content={con.item3} />
+            </CardDeck>
+            <CardDeck>
+              <CardBox content={con.item1} />
+              <CardBox content={con.item2} />
+              <CardBox content={con.item3} />
+            </CardDeck>
+          </Container>
+        </div>
       </div>
     );
   }
 }
 
-function TestTitle(props) {
-  return <h1>{props.title}</h1>;
+function CardBox(props) {
+  return (
+    <Card>
+      <Card.Body>
+        <Card.Title>{props.content.title}</Card.Title>
+        <Card.Body>{props.content.body}</Card.Body>
+        <Card.Footer>{props.content.footer}</Card.Footer>
+      </Card.Body>
+    </Card>
+  );
 }
-
-const testf = () => {};
